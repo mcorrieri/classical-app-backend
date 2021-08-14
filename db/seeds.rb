2e361b6dbcd066a502675d11a6193d3f0dc79a02
Michael <Mlcorrieri@gmail.com>
2021-08-13 22:07:38 -0400

    users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 
-# TESTING
+puts "Creating Users"
+u1 = User.create(name: 'Duey', username: "theDTP", email: 'Duey@TP.coolkid.com')
+
+puts "Done!!!"
