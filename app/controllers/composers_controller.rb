332d3b64da20755260200c64f5df2db1c7412280
Michael <Mlcorrieri@gmail.com>
2021-08-30 17:06:14 -0400

    composer actions added

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -1,2 +1,18 @@
 class ComposersController < ApplicationController
+
+    def index
+        @composers = Composer.all
+        render json: @composers
+    end
+
+    def show
+        @composer = Composer.find(params[:id])
+        render json :@composer
+    end
+
+    private
+    def composer_params
+        params.require(:composer).permit(:name, :full_name, :born, :died, :genre, :bio)
+    end
+
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,48 @@
 class FavoritesController < ApplicationController
 
-    
+ def index
+    @favorites = Favorite.all
+    render json: @favorites
+  end
 
+  def show
+    render json: @favorite
+  end
 
+  def new
+    @favorite = Favorite.new
+    render json: @favorite
+  end
 
+  def edit
+  end
 
+  def create
+    @favorite = Favorite.new(favorite_params)
+      if @favorite.save
+        render json: @favorite
+      else
+        render json: {error: "Invalid favorite"}
+      end
+  end
+  
 
+#   def update
+#       if @favorite.update(favorite_params)
+#        render json: @favorite
+#       else
+#         render json: {error: "Could not update favorite"}
+#       end
+#   end
+  
 
+  def destroy
+    @favorite.destroy
+  end
 
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :composer_id)
+  end
 end
